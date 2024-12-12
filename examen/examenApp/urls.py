854fefe56d7ed5821782e98b478575ca2b01dbbd
Fran Rivera <francisco.rivera.gil.al@iespoligonosur.org>
2024-12-12 11:12:58 +0100

    terminado

--- examen/examenApp/urls.py
+++ examen/examenApp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('promociones/editar/<int:pk>/', views.editar_promocion, name='editar_promocion'),  # Editar promoción
     path('promociones/eliminar/<int:pk>/', views.eliminar_promocion, name='eliminar_promocion'),  # Eliminar promoción
 
-    # Búsqueda avanzada de promociones
+    # Busqueda avanzada de promociones
     path('promociones/buscar/', views.buscar_promociones, name='buscar_promociones'),
 ]
